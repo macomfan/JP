cb2fd04af4aed10fdd8d0bfe9fe1ac882ac8f0d8
macomfan <macomfan@163.com>
2017-12-07 16:40:23 +0800

    Test slave

--- TestSlave/MainUI/TestSlaveFrame.java
+++ TestSlave/MainUI/TestSlaveFrame.java
@@ -5,17 +5,52 @@
  */
 package MainUI;
 
+import JPWord.Data.Database;
+import JPWord.Data.IWordDictionary;
+import JPWord.File.DefaultFileReader;
+import JPWord.File.DefaultFileWriter;
+import JPWord.Synchronizer.ILogging;
+import JPWord.Synchronizer.Sync;
+import java.io.File;
+
 /**
  *
  * @author u0151316
  */
 public class TestSlaveFrame extends javax.swing.JFrame {
 
+    class WorkerClass extends Thread {
+
+        public TestSlaveFrame p_ = null;
+
+        @Override
+        public void run() {
+            try {
+                ILogging logging = Sync.getInstance().getLogging();
+                while (true) {
+                    String msg = logging.pop();
+                    if (msg != null) {
+                        String log = p_.jtxtLog.getText();
+                        log += msg;
+                        log += "\r\n";
+                        p_.jtxtLog.setText(log);
+                    }
+                    Thread.sleep(10);
+                }
+            } catch (Exception e) {
+            }
+
+        }
+    }
+
     /**
      * Creates new form TestSlaveFrame
      */
     public TestSlaveFrame() {
         initComponents();
+        WorkerClass worker = new WorkerClass();
+        worker.p_ = this;
+        worker.start();
     }
 
     /**
@@ -26,21 +61,57 @@ public class TestSlaveFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jbtnStartSlave = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jtxtLog = new javax.swing.JTextArea();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jbtnStartSlave.setText("Start Slave");
+        jbtnStartSlave.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnStartSlaveActionPerformed(evt);
+            }
+        });
+
+        jtxtLog.setColumns(20);
+        jtxtLog.setRows(5);
+        jScrollPane1.setViewportView(jtxtLog);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 673, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jbtnStartSlave)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jbtnStartSlave)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 268, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jbtnStartSlaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnStartSlaveActionPerformed
+        // TODO add your handling code here:
+        File file = new File("C:\\Users\\u0151316\\Documents\\JP\\Dictionary_Slave.dat");
+        DefaultFileReader reader = new DefaultFileReader(file);
+        DefaultFileWriter writer = new DefaultFileWriter(file);
+        IWordDictionary dict = Database.createWordDictionary(reader, writer);
+        Sync.getInstance().startAsSlave(dict, Sync.Method.SYNC_FROM_MASTER);
+    }//GEN-LAST:event_jbtnStartSlaveActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +149,8 @@ public class TestSlaveFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton jbtnStartSlave;
+    private javax.swing.JTextArea jtxtLog;
     // End of variables declaration//GEN-END:variables
 }
